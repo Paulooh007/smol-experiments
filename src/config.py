9b8f883acd51a67cf490f94fe7ccf3818cb9cf3b
Paul Okewunmi <okewunmipaul@yahoo.com>
2025-12-04 02:36:17 +0100

    Add expert map for expert specialization exp

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 class SmolConfig:
+    """Base configuration for SmolLM-135M"""
     vocab_size = 49152
     hidden_size = 576
     intermediate_size = 1536
@@ -7,5 +8,13 @@ class SmolConfig:
     kv_heads = 3
 
 class SmolMoEConfig(SmolConfig):
+    """Configuration for the Upcycled MoE Model"""
     num_experts = 3
     num_experts_per_tok = 1
+
+    expert_map = {
+        0: [0], # Code -> Expert 0
+        1: [1], # Math -> Expert 1
+        2: [2], # Chat -> Expert 2
+    }
+    low_p = 0.001 # Probability for non-targeted experts
